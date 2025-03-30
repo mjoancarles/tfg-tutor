d5410c6c948d7e9bddd4252bf202e7703f41bfdd
Joan Carles Montero <joan.carles.montero@gmail.com>
2025-03-31 00:30:42 +0200

    rag graph in utils

--- app/pages/part1.py
+++ app/pages/part1.py
@@ -4,13 +4,7 @@ import os
 import streamlit as st
 import logging
 from utils import * # Import the utils.py functions
-from langchain_core.documents import Document
-from langchain_core.prompts.base import format_document
-from typing_extensions import List, TypedDict
-from langgraph.graph import START, StateGraph, MessagesState, END
 from langchain_core.messages import HumanMessage, BaseMessage, AIMessage, SystemMessage
-from langchain_core.prompts import PromptTemplate
-from langgraph.checkpoint.memory import MemorySaver
 
 st.set_page_config(
     page_title="TFG Tutor Chatbot",
@@ -37,79 +31,10 @@ langfuse_handler = CallbackHandler()
 # https://python.langchain.com/docs/integrations/vectorstores/qdrant/
 vector_store = get_qdrant_vector_store(embeddings)
 
-def initialize_app():
-
-    template = """
-    General Info:
-    You are an assistant answering questions about TFGs (Final Degree Projects by Catalan students) and CVC scientific publications. The CVC is a non-profit research center established in 1995 by the Generalitat de Catalunya and the UAB, focusing on computer vision research and collaborating on TFGs.
-
-    Instructions:
-
-    -When Responding:
-    Only include retrieved documents if they are relevant to the query. If no relevant documents are found, answer directly.
-    -Document Formatting:
-    For Publications:
-        Title: (publication title)
-        - Authors: (comma-separated list)
-        - Abstract: (summarized abstract)
-        - Published in: (year)
-        - Link: (if available)
-    For TFGs:
-        Title: (TFG title)
-        - Authors: (comma-separated list)
-        - Abstract: (summarized abstract)
-        - Delivered in: (year)
-        - Link: (if available)
-    Exclude any field that is NULL/EMPTY. Show only the top 5 most relevant results by default (unless the user requests more). After listing, include a brief explanation and comparison of the documents, highlighting key details in bold.
-    Direct Answers:
-    If the query does not require listing documents, answer directly without showing retrieved documents. Always answer in english.
-    ------------------------------
-    Retrieved documents: {context}
-        """
-    prompt = PromptTemplate.from_template(template)
-
-    document_prompt = PromptTemplate.from_template(
-                "Title: {title}\n"
-                "Type: {type}\n"
-                "Abstract: {page_content}\n"
-                "Authors: {authors}\n"
-                "Year: {year}\n"
-                "Keywords: {keywords}\n"
-                "Link: {link}\n"
-    )
-
-    # https://python.langchain.com/docs/tutorials/rag/
-    class State(TypedDict):
-        messages: List[BaseMessage]
-        context: List[Document]
-
-    def retrieve(state: State):
-        retrieved_docs = vector_store.similarity_search(state["messages"][-1].content)
-        return {"context": retrieved_docs}
-
-    def generate(state: State):
-        messages = state["messages"]
-        print(messages)
-        #docs_content = "\n\n".join(doc.page_content for doc in state["context"])
-        docs_content = "\n\n".join(format_document(doc, document_prompt) for doc in state["context"])
-        system_prompt = prompt.invoke({"context": docs_content}).to_string()
-        #print(system_prompt)
-        messages = [SystemMessage(system_prompt)] + messages
-        print(messages)
-        response = llm.invoke(messages)
-        #print(type(response)) # <class 'langchain_core.messages.ai.AIMessage'>
-        return {"messages": [response]}
-
-    graph_builder = StateGraph(State).add_sequence([retrieve, generate])
-    graph_builder.add_edge(START, "retrieve")
-    graph_builder.add_edge("generate", END)
-    graph = graph_builder.compile()
-    return graph
-
 def main():
     # Ensure 'graph' is created only once per session
     if "graph" not in st.session_state:
-        st.session_state.graph = initialize_app()
+        st.session_state.graph = get_rag_graph(llm, vector_store)
 
     # We’ll use a local variable for convenience
     graph = st.session_state.graph
